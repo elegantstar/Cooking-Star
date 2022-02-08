2548bd92b4c3ea809f80a4aabaf17245e5884ec2
elegantstar <elegant_star@naver.com>
2022-02-08 11:21:48 +0900

    reflected PR comments in feature/edit-profile-image

--- src/main/java/toy/cookingstar/service/imagestore/ImageStoreService.java
+++ src/main/java/toy/cookingstar/service/imagestore/ImageStoreService.java
@@ -22,12 +22,12 @@ public class ImageStoreService {
     private String profileDir;
 
     //이미지 저장 경로 찾기
-    public String getFullPath(ImageType imageType, String url) {
+    public String getFullPath(ImageType imageType, String uri) {
         String dirPath = getDirPath(imageType);
         if (StringUtils.isEmpty(dirPath)) {
             return null;
         }
-        return dirPath + url.substring(0, 10) + "/" + url;
+        return dirPath + uri.substring(0, 10) + "/" + uri;
     }
 
     //현재 날짜 로직
@@ -74,10 +74,11 @@ public class ImageStoreService {
 
     //이미지 저장 경로 결정
     private String getDirPath(ImageType imageType) {
-        if (imageType == ImageType.POST) {
-            return imageDir;
-        } else if (imageType == ImageType.PROFILE) {
-            return profileDir;
+        switch (imageType) {
+            case POST:
+                return imageDir;
+            case PROFILE:
+                return profileDir;
         }
         return null;
     }
